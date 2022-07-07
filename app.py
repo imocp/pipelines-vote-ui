9f299ae6621dacfb4df3ec5e3b815fc95a16b77f
viyancs <msofyancs@gmail.com>
2022-07-07 14:12:00 +0700

    change replica to 5

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 3333")
+option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 4444")
 option_b = os.getenv('OPTION_B', u"" )
 hostname = socket.gethostname()
 
