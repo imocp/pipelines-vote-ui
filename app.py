e43d578189ad6f20fae426d5e60cb9c1b955b077
viyancs <msofyancs@gmail.com>
2022-07-13 08:50:52 +0700

    change wording

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 1111")
+option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 2222")
 option_b = os.getenv('OPTION_B', u"" )
 hostname = socket.gethostname()
 
