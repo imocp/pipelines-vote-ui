2d4ddcc0f054039dd8f63e8cac23771ed4ca4880
immgcb <32258697+immgcb@users.noreply.github.com>
2022-09-07 15:38:53 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import random
 import json
 import requests
 
-option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 8888888888888")
+option_a = os.getenv('OPTION_A', u"CI CD (Tekton) Live Demo Test # 23534764578548569")
 option_b = os.getenv('OPTION_B', u"" )
 hostname = socket.gethostname()
 
